46e2cc74ac9602b5b6c2f22457285393de065e2a
Jason <czjchn@163.com>
2016-10-09 16:24:50 +0800

    Create demo7.js

---
+++ es6/demo7.js
@@ -0,0 +1,6 @@
+var foo = {name: 'www'};
+var goo = {age: 13};
+var koo = Object.assign({}, foo, goo);
+var loo = {...foo, ...goo};
+//Object.assign babel转换后还是原来的样子 如果有写浏览器不支持（比如微信内核X5）就会报错
+// ...这个是stage-0 es7会进行转换
